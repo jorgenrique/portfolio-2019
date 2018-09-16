b7c4d4fc483e61964db2333e3b395f94c2f7721d
Jorge <jorse@beethedata.com>
2018-09-16 22:58:22 +0200

    design direction change

--- js/main.js
+++ js/main.js
@@ -1,120 +1,13 @@
-$(document).ready( function () {
+/*
+const aboutMeBtn = document.querySelector('.about-me-btn')
+const aboutMeClose = document.querySelector('.about-me-close')
+const aboutMe = document.querySelector('.about-me')
 
-    var body = $("html, body"),
-        aboutOffset = $('.about-me').offset().top,
-        workOffset = $('.how-i-work').offset().top,
-        // caseOffset = $('.case-studies').offset().top,
-        contactMe = $('.contact-me').offset().top,
-        slideWidth = $('.how-i-work .slides-container').width(),
-        slideElements = $('.how-i-work .slide'),
-        slideGallery = $('.how-i-work .slide-gallery'),
-        slideNumber = 1,
-        header = $('header')
-
-    $(window).scroll(function() {
-
-        var scroll = $(window).scrollTop();
-
-        if ( $(window).width() > 1024 ) {
-
-            if (scroll >= aboutOffset) {
-
-                $(".header").addClass("fixedHeader");
-
-            } else {
-
-                $(".header").removeClass("fixedHeader");
-            }
-        }
-    })
-
-
-    slideElements.css( 'width', slideWidth )
-    slideGallery.css('width', ( ( slideWidth + 8 ) * slideElements.size() ) )
-
-    $('.arrow-right').on('click', function() {
-
-        if (slideNumber == 1) { $('.arrow-left').addClass('show') }
-
-        slideNumber ++
-
-        if ( slideNumber < slideElements.size() ) {
-
-            slideGallery.css('marginLeft', ( - slideWidth * ( slideNumber - 1 ) ) )
-
-        } else if ( slideNumber = slideElements.size() ) {
-
-            $(this).removeClass('show')
-            slideGallery.css('marginLeft', ( - slideWidth * ( slideNumber - 1 ) ) )
-        }
-    })
-
-
-    $('.arrow-left').on('click', function() {
-
-        if (slideNumber == slideElements.size() ) { $('.arrow-right').addClass('show') }
-
-        slideNumber --
-
-        if ( slideNumber > 1 ) {
-
-            slideGallery.css('marginLeft', ( - slideWidth * ( slideNumber - 1 ) ) )
-
-        } else if ( slideNumber = 1  ) {
-
-            $(this).removeClass('show')
-            slideGallery.css('marginLeft', ( - slideWidth * ( slideNumber - 1 ) ) )
-        }
-    })
-
-    $('.arrow-more').on('click', function() {
-
-        body.animate({ scrollTop: aboutOffset }, 600);
-    })
-
-    $('.link-1').on('click', function() {
-
-        body.animate({ scrollTop: aboutOffset }, 600);
-    })
-
-    $('.link-2').on('click', function() {
-
-        body.animate({ scrollTop: workOffset }, 600);
-    })
-
-    $('.link-3').on('click', function() {
-
-        body.animate({ scrollTop: caseOffset }, 600);
-    })
-
-    $('.link-4').on('click', function() {
-
-        body.animate({ scrollTop: contactMe }, 600);
-    })
-
-
-
-    $('.btn').on('click', function(){
-
-        $('.footer').toggleClass('open')
-        $(this).addClass('grow')
-    })
-
-    $('.btn').on('transitionend webkitTransitionEnd oTransitionEnd', function () {
-
-        $(this).removeClass('grow')
-
-    });
-
-
-    $('.years-cont li').on('click', function() {
-
-        var yearId = $(this).attr('id')
-        var showYear = $('.' + yearId)
-
-        $('.years-cont li').removeClass('active')
-        $('.timeline .main-info ul.show').removeClass('show')
-        $(this).addClass('active')
-        showYear.addClass('show')
-    })
+aboutMeBtn.addEventListener('click', function () {
+    aboutMe.classList.add('show')
 })
+
+aboutMeClose.addEventListener('click', function () {
+    aboutMe.classList.remove('show')
+})
+*/
