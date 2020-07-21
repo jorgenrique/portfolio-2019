17e21e78d245ab56f18fd17ec27d7d04239e219d
Jorge Giraldo <jorgegiraldo@Jorges-MBP-2.home>
2020-07-21 21:31:26 +0200

    update portfolio

--- development/js/main.js
+++ development/js/main.js
@@ -1,109 +1,94 @@
-const navButtons = document.querySelectorAll('.nav-option')
-const header = document.querySelector('.header')
-const experience = document.querySelector('.experience')
-const skills = document.querySelector('.skills')
-const products = document.querySelector('.products')
-const brands = document.querySelector('.brands')
+const navButtons = document.querySelectorAll('.nav-option');
+const header = document.querySelector('.header');
+const experience = document.querySelector('.experience');
+const skills = document.querySelector('.skills');
+const products = document.querySelector('.products');
+const brands = document.querySelector('.brands');
 
-let scrollpos = window.scrollY
-let eflag = true
-let sflag = false
-let pflag = false
-let bflag = false
+let scrollpos = window.scrollY;
+let eflag = true;
+let sflag = false;
+let pflag = false;
+let bflag = false;
 
-const windowHeight = window.innerHeight
-const docHeight = document.documentElement.scrollHeight
+const windowHeight = window.innerHeight;
 
-const eOffset = offset(experience).top - (windowHeight - 200)
-const sOffset = offset(skills).top - (windowHeight - 200)
-const pOffset = offset(products).top - (windowHeight - 200)
-const bOffset = offset(brands).top - (windowHeight - 200)
-const bottom = docHeight - windowHeight
+const expbtn = document.querySelector('.nav-option[data-id="experience"]');
+const skibtn = document.querySelector('.nav-option[data-id="skills"]');
+const probtn = document.querySelector('.nav-option[data-id="products"]');
+const brabtn = document.querySelector('.nav-option[data-id="brands"]');
 
-const expbtn = document.querySelector(`.nav-option[data-id="experience"]`)
-const skibtn = document.querySelector(`.nav-option[data-id="skills"]`)
-const probtn = document.querySelector(`.nav-option[data-id="products"]`)
-const brabtn = document.querySelector(`.nav-option[data-id="brands"]`)
-
-
-// Functions
 
 function offset(el) {
-
-    var rect = el.getBoundingClientRect(),
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { top: rect.top + scrollTop }
+  const rect = el.getBoundingClientRect();
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop };
 }
+
+
+const eOffset = offset(experience).top - (windowHeight - 200);
+const sOffset = offset(skills).top - (windowHeight - 200);
+const pOffset = offset(products).top - (windowHeight - 200);
+const bOffset = offset(brands).top - (windowHeight - 200);
 
 
 function runOnScroll() {
+  scrollpos = window.scrollY;
 
-    scrollpos = window.scrollY;
+  if (scrollpos >= 200) {
+    header.classList.add('show');
+  } else {
+    header.classList.remove('show');
+  }
 
-    if (scrollpos >= 200) {
-        header.classList.add('show')
-    } else {
-        header.classList.remove('show')
+  if (scrollpos >= eOffset && scrollpos <= sOffset) {
+    if (eflag) {
+      navButtons.forEach((navButton) => navButton.classList.remove('selected'));
+      expbtn.classList.add('selected');
     }
 
-    if ( scrollpos >= eOffset && scrollpos <= sOffset) {
-
-        if (eflag) {
-            navButtons.forEach(navButton => navButton.classList.remove('selected'))
-            expbtn.classList.add('selected')
-        }
-
-        eflag = false;
-        sflag = true;
-
-    } else if ( scrollpos >= sOffset && scrollpos <= pOffset ) {
-
-        if (sflag) {
-            navButtons.forEach(navButton => navButton.classList.remove('selected'))
-            skibtn.classList.add('selected')
-        }
-
-        sflag = false;
-        pflag = true;
-        eflag = true;
-
-    } else if ( scrollpos >= pOffset && scrollpos <= bOffset ) {
-
-        if (pflag) {
-            navButtons.forEach(navButton => navButton.classList.remove('selected'))
-            probtn.classList.add('selected')
-        }
-
-        pflag = false;
-        bflag = true;
-        sflag = true;
-
-    } else if ( scrollpos >= bOffset ) {
-
-        if (bflag) {
-            navButtons.forEach(navButton => navButton.classList.remove('selected'))
-            brabtn.classList.add('selected')
-        }
-
-        bflag = false;
-        pflag = true;
-
-    } else {
-
-        navButtons.forEach(navButton => navButton.classList.remove('selected'))
-        eflag = true;
-        sflag = false;
+    eflag = false;
+    sflag = true;
+  } else if (scrollpos >= sOffset && scrollpos <= pOffset) {
+    if (sflag) {
+      navButtons.forEach((navButton) => navButton.classList.remove('selected'));
+      skibtn.classList.add('selected');
     }
-};
 
+    sflag = false;
+    pflag = true;
+    eflag = true;
+  } else if (scrollpos >= pOffset && scrollpos <= bOffset) {
+    if (pflag) {
+      navButtons.forEach((navButton) => navButton.classList.remove('selected'));
+      probtn.classList.add('selected');
+    }
 
-function scrollToSection () {
+    pflag = false;
+    bflag = true;
+    sflag = true;
+  } else if (scrollpos >= bOffset) {
+    if (bflag) {
+      navButtons.forEach((navButton) => navButton.classList.remove('selected'));
+      brabtn.classList.add('selected');
+    }
 
-    const id = this.getAttribute('data-id')
-    const section = '.'+id
-    document.querySelector(section).scrollIntoView({ behavior: 'smooth' })
+    bflag = false;
+    pflag = true;
+  } else {
+    navButtons.forEach((navButton) => navButton.classList.remove('selected'));
+    eflag = true;
+    sflag = false;
+  }
 }
 
 
-navButtons.forEach(navButton => navButton.addEventListener('click', scrollToSection))
-window.addEventListener("scroll", runOnScroll);
+function scrollToSection() {
+  const id = this.getAttribute('data-id');
+  const section = `.${id}`;
+  document.querySelector(section).scrollIntoView({ behavior: 'smooth' });
+}
+
+
+navButtons.forEach((navButton) => navButton.addEventListener('click', scrollToSection));
+window.addEventListener('scroll', runOnScroll);
